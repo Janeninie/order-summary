8ffdf90ee1b16348cf7eab089d802580417eefe9
Jane <apiwatsupaphol@gmail.com>
2024-12-15 16:35:28 +0700

    feat: added order card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,100 +2,62 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className="h-screen flex items-center justify-center bg-mobile sm:bg-desktop bg-no-repeat bg-contain bg-primary-paleBlue">
+      <section className="flex flex-col w-96 bg-white rounded-xl shadow-card">
+        <div className="">
+          <Image
+            src={"/Images/illustration-hero.svg"}
+            alt="Illustration"
+            width={450}
+            height={220}
+            className="rounded-t-xl"
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="flex flex-col items-center p-10 gap-4 ">
+          <h1 className="font-extrabold text-2xl text-second-darkBlue">
+            Order Summary
+          </h1>
+          <p className="text-sm text-second-desaturatedBlue text-center">
+            You can now listen to millions of songs, audiobooks, and podcasts on
+            any device anywhere you like!
+          </p>
+          <div className="w-full rounded-xl bg-second-veryPaleBlue flex items-center justify-between p-4">
+            <div className="flex gap-4">
+              <div>
+                <Image
+                  src={"/images/icon-music.svg"}
+                  alt="Music Icon"
+                  width={40}
+                  height={40}
+                />
+              </div>
+              <div className="flex flex-col gap-0.5">
+                <p className="text-sm font-extrabold text-second-darkBlue">
+                  Annual Plan
+                </p>
+                <p className="text-xs text-second-desaturatedBlue">
+                  $59.99/year
+                </p>
+              </div>
+            </div>
+            <div>
+              <button className="text-xs font-extrabold underline text-primary-brightBlue ">
+                Change
+              </button>
+            </div>
+          </div>
+          <div className="w-full flex items-center">
+            <button className="w-full rounded-xl bg-primary-brightBlue shadow-button text-sm font-semibold text-white p-2.5">
+              Proceed to Payment
+            </button>
+          </div>
+          <div className="w-full flex items-center justify-center">
+            <button className="text-xs font-extrabold text-second-desaturatedBlue p-1">
+              Cancel Order
+            </button>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,23 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: {
+          paleBlue: "hsl(225, 100%, 94%)",
+          brightBlue: "hsl(245, 75%, 52%)",
+        },
+        second: {
+          veryPaleBlue: "hsl(225, 100%, 98%)",
+          desaturatedBlue: "hsl(224, 23%, 55%)",
+          darkBlue: "hsl(223, 47%, 23%)",
+        },
+      },
+      backgroundImage: {
+        desktop: "url('/images/pattern-background-desktop.svg')",
+        mobile: "url('/images/pattern-background-mobile.svg')",
+      },
+      boxShadow: {
+        button: "0px 10px 20px hsla(223, 47%, 23%, 0.3)",
+        card: "0px 10px 30px hsla(223, 47%, 23%, 0.3)",
       },
     },
   },
